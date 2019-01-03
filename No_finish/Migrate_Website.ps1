3417281f9b76fd57003692184dfad7a272cea34d
yerick81 <yerickson.arias@globant.com>
2019-01-03 16:32:45 -0300

    Update Migrate_Website.ps1

--- No_finish/Migrate_Website.ps1
+++ No_finish/Migrate_Website.ps1
@@ -1,17 +1,34 @@
 #Set variables
+$Site = 'directv.com.co' 
 $Configfolder = 'C:\Windows\System32\inetsrv\backup'
-$BackupFolder = 'C:\Sources'
-$SiteFolder = 'C:\inetpub\wwwroot\Caribbean'
-$NewSiteFolder = 'C:\Sources\Caribbean'
-Start-Transcript -Path C:\Sources\MigrationLog.txt -NoClobber
+$BackupFolder = 'D:\SecureSite\Backup_BeforeSecure\Colombia'
+$SiteFolder = 'D:\Deployment\PublicSite\Colombia'
+$NewSiteFolder = 'D:\SecureSite\Secure_Sources\Colombia\*'
+Start-Transcript -Path D:\SecureSite\Backup_BeforeSecure\Colombia\MigrationLog.txt -NoClobber
+#Stopping and disabling applications
+Write-host "Disabling the tasks and processes..." -Foregroundcolor Red
+Stop-ScheduledTask -taskname "SyncDirectvlaFromMasterDrive"
+Stop-ScheduledTask -taskname "SyncOnDirectvFromMasterDrive"
+Stop-ScheduledTask -taskname "SyncPublicSiteFromMasterDrive"
+Disable-ScheduledTask -TaskName "AppPoolStart"
+Disable-ScheduledTask -TaskName "SyncDirectvlaFromMasterDrive"
+Disable-ScheduledTask -TaskName "SyncOnDirectvFromMasterDrive"
+Disable-ScheduledTask -TaskName "SyncPublicSiteFromMasterDrive"
+Stop-Process -name "Robocopy"
 Import-Module IISAdministration
 #Stopping application pool
-Stop-IISSite -Name "Carebbean" -Confirm
+Stop-IISSite -Name "$Site" -Confirm
+Write-Host "The Application pool $Site is DOWN" -ForegroundColor Red
 #Making backup
-Backup-WebConfiguration -Name "Carebbean" 
+Write-Host "Making backup of the site $Site..." -ForegroundColor Green
+Backup-WebConfiguration -Name "$Site" 
 Copy-Item -Path $ConfigFolder -Destination $BackupFolder -Recurse
 Copy-item -Path $SiteFolder -Destination $BackupFolder -Recurse
-Remove-Item -Path $SiteFolder* -Recurse -Force
-Copy-item -Path $NewSiteFolder -Destination C:\inetpub\wwwroot -Recurse
-Start-IISSite -Name "Carebbean"
+Write-Host "The backup has finished successfully" -ForegroundColor Green
+Write-Host "Wait I am migrating the site..." -ForegroundColor Green
+Remove-Item -Path $SiteFolder\* -Recurse -Force
+Copy-item "$NewSiteFolder" -Destination "$SiteFolder" -Recurse
+Start-IISSite -Name "$Site"
+Write-Host "The Application pool $Site is UP" -ForegroundColor Green
+Write-Host "The Site $Site has been migrated successfully" -ForegroundColor Green
 Stop-Transcript
